bc536cacd533e49bd9ea6b18b67ba3f710fc30dc
Jaypatel0924 <jayshubham2324@gmail.com>
2025-05-16 17:28:07 +0530

    server.js update

--- server/server.js
+++ server/server.js
@@ -8,54 +8,59 @@ import messageRouter from './routes/messageRoutes.js';
 import { Server } from 'socket.io';
 
 // Create Express app using HTTP Server
-const app= express();
+const app = express();
 
 const server = http.createServer(app);
 
 // Initialize socket.io server
-export const io = new Server(server,{
-    cors:{origin:"*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 
 // Store online users
 export const userSocketMap = {}; // {userId: socketId}
 
 // socket.io connection handler
-io.on("connection",(socket) => {
-    const userId= socket.handshake.query.userId;
-    console.log("User connected "+userId);
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected " + userId);
 
-    if(userId)
-    {
-        userSocketMap[userId]=socket.id;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
     }
     // Emit online users to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    socket.on("disconnect",() =>{
-        console.log("User disconnected  "+userId);
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    socket.on("disconnect", () => {
+        console.log("User disconnected  " + userId);
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
-    
+
 })
 
 // Middleware Setup
-app.use(express.json({limit : "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // Route setup
-app.use("/api/status",(req,res) => {
+app.use("/api/status", (req, res) => {
     res.send("Server is live");
 })
 
-app.use("/api/auth",userRouter);
-app.use("/api/messages",messageRouter);
-
-const PORT =process.env.PORT || 5000;
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 // Connect to mongodb
 await connectDB();
 
-server.listen(PORT, () => {
-    console.log("Server is running on port "+PORT);
-})
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+
+    server.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+    })
+}
+
+// Export the server for vercel
+export default server;
+
